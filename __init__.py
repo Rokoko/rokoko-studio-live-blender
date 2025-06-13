a9a2b872d242f34c098c1860a5fc729ce2bf5076
Philipp Köhler <philipp.koehler24@web.de>
2025-06-13 22:42:37 +0200

    Ready for 1.4.2

--- __init__.py
+++ __init__.py
@@ -5,12 +5,12 @@ bl_info = {
     'category': 'Animation',
     'location': 'View 3D > Tool Shelf > Rokoko',
     'description': 'Stream your Rokoko Studio animations directly into Blender',
-    'version': (1, 4, 1),
+    'version': (1, 4, 2),
     'blender': (2, 80, 0),
     'wiki_url': 'https://github.com/Rokoko/rokoko-studio-live-blender#readme',
 }
 
-beta_branch = True
+beta_branch = False
 
 first_startup = "bpy" not in locals()
 import bpy
